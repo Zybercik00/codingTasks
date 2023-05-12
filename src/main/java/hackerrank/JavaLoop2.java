1e294d9eb7a06237c4bdd7894277c56e4e64f369
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-05-12 10:52:32 +0200

    corrections

--- src/main/java/hackerrank/JavaLoop2.java
+++ src/main/java/hackerrank/JavaLoop2.java
@@ -6,19 +6,27 @@ public class JavaLoop2 {
 
     public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    int n = scanner.nextInt();
-    int a = scanner.nextInt();
-    int b = scanner.nextInt();
-    int pow = 0;
+        Scanner scanner = new Scanner(System.in);
 
-    int currentTerm = (int) (a + Math.pow(2, pow) * b);
-            for (int i = 0; i < n; i++) {
-        currentTerm += (int) Math.pow(2, pow++) * b;
-        System.out.print(currentTerm + " ");
-    }
+        int q = scanner.nextInt();
+        for (int i = 0; i < q; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int n = scanner.nextInt();
+
+
+
+            int pow = 0;
+            int currentTerm = (int) (a + pow * b);
+            for (int j = 0; j < n; j++) {
+                int powResult = (int) Math.pow(2, pow++);
+                currentTerm += (int) powResult * b;
+                System.out.print(currentTerm + " ");
+            }
             System.out.println();
-            scanner.close();
+
+        }
+
+    }
 }
 
-}
