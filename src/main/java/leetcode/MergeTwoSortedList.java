2bbbf9b98a396a3ca8db0cdd08fc08304712b1df
Kamil Chmiel <kamil.chmiel@icloud.com>
2023-10-17 14:08:22 +0200

    training

---
+++ src/main/java/leetcode/MergeTwoSortedList.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class MergeTwoSortedList {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode mergedList = new ListNode(0);
+        ListNode current = mergedList;
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+        current.next = list1 != null ? list1:list2;
+        return mergedList.next;
+    }
+}
